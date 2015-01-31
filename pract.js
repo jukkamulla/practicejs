95804e947536cc502ba020a37f97794df9bfccc8
jukkamulla <jukkamulla@gmail.com>
2015-01-31 22:23:10 +0100

    add paragraph to divs

--- pract.js
+++ pract.js
@@ -1,24 +1,32 @@
 function createDivWithStyle(backgroundColor) {
     var div = document.createElement("div");
     div.style.width = "100px";
-    div.style.height = "50px";
+    div.style.height = "100px";
+    div.style.borderRadius = "50px";
     div.style.backgroundColor = backgroundColor;
+    var paragraph = document.createElement("p");
+    div.appendChild(paragraph);
+    paragraph.innerHTML = backgroundColor;
     return div;
 }
 
 function createDivWithCssClass(cssClass) {
     var div = document.createElement("div");
     div.classList.add(cssClass);
+    div.style.borderRadius = "50px";
+    var paragraph = document.createElement("p");
+    div.appendChild(paragraph);
+    paragraph.innerHTML = cssClass;
     return div;
 }
 
 var yellowDiv = createDivWithCssClass("yellow");
 var redDiv = createDivWithStyle("red");
-var blackDiv = createDivWithStyle("black");
+var blueDiv = createDivWithStyle("blue");
 var greenDiv = createDivWithStyle("green");
 var test = document.getElementById("test");
 test.appendChild(yellowDiv);
 test.appendChild(redDiv);
-test.appendChild(blackDiv);
+test.appendChild(blueDiv);
 test.appendChild(greenDiv);
 
